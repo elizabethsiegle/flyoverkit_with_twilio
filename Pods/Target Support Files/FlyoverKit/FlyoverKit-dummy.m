6ad6c1aa937315e45ff4b86a2910fd6b15b9f468
Lizzie Siegle <esiegle@brynmawr.edu>
2018-10-11 01:25:12 -0700

    gitignore fixed idk man

--- Pods/Target Support Files/FlyoverKit/FlyoverKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FlyoverKit : NSObject
-@end
-@implementation PodsDummy_FlyoverKit
-@end
--- Pods/Target Support Files/Pods-TwilioFlyoverTests/Pods-TwilioFlyoverTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TwilioFlyoverTests : NSObject
-@end
-@implementation PodsDummy_Pods_TwilioFlyoverTests
-@end
